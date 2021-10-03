da971b92b481a31a2e7167976974c8b0f62342e3
yathin1405 <yathz14@gmail.com>
2021-10-03 22:47:20 +0200

    new

---
+++ OlwandleHotel/Controllers/FeedbacksController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OlwandleHotel.Models;
+using System.Threading.Tasks;
+
+namespace OlwandleHotel.Controllers
+{
+    public class FeedbacksController : Controller
+    {
+        ApplicationDbContext context;
+
+        public FeedbacksController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: Feedback
+        public ActionResult Index()
+        {
+            return View(context.Feedbacks.ToList());
+        }
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(FeedbackViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                return RedirectToAction("Index1");
+
+            }
+            model.Answers = Common.GetAnswers();
+            return RedirectToAction("Index1");
+        }
+    }
+}
